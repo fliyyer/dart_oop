6c7a949e83f7f8a38b62a66678c38470b43e0738
user.email <user.name>
2025-06-21 10:59:45 +0700

    method

---
+++ bin/3_method.dart
@@ -0,0 +1,17 @@
+void main(List<String> args) {
+  Car car = Car();
+
+  // pemanggulan method pada class
+  car.road();
+}
+
+class Car {
+  String brand = "Unknown";
+  final int wheel = 4;
+  String? color;
+
+  // Method
+  void road() {
+    print("Ngengggggg....ngenggggg");
+  }
+}
